bd64e57205334409aa9a5935776e757c94d3a217
Hizkia Crisantino <hcrisantino@gmail.com>
2026-06-09 18:16:01 +0700

    style: apply prettier formatting

--- backend/app.js
+++ backend/app.js
@@ -13,8 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.status(200).send("Backend is running");
+app.get('/', (req, res) => {
+  res.status(200).send('Backend is running');
 });
 
 app.get('/api/v1/health', (req, res) => {
