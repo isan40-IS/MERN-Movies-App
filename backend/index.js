937d8d4ea9bb594a576dbc65c591aa49af1de1d1
CowCatt <162095280+CowCatt@users.noreply.github.com>
2026-06-09 18:15:33 +0700

    feat: Prometheus and Grafana monitoring

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import app from './app.js';
+import client from 'prom-client';
 
 dotenv.config();
 connectDB();
@@ -8,3 +9,11 @@ connectDB();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics({ register: client.register });
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
