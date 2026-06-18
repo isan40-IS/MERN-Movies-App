4fe8262fa43e60674eab3f1bf814cba64ad21090
CowCatt <162095280+CowCatt@users.noreply.github.com>
2026-06-18 16:46:45 +0700

    fix: fixed cors

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import path from 'path';
+import cors from 'cors';
 import client from 'prom-client';
 
 import userRoutes from './routes/userRoutes.js';
@@ -9,6 +10,22 @@ import moviesRoutes from './routes/moviesRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 
 const app = express();
+
+const allowedOrigins = ['http://localhost:5173'];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      // Tolak origin yang tidak ada di daftar
+      if (allowedOrigins.indexOf(origin) === -1) {
+        return callback(new Error('Not allowed by CORS'), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -27,27 +44,13 @@ const httpRequestDurationMicroseconds = new client.Histogram({
   buckets: [0.1, 0.3, 0.5, 0.7, 1, 3, 5, 7, 10],
 });
 
-//Tracking middleware
 app.use((req, res, next) => {
   const endTimer = httpRequestDurationMicroseconds.startTimer();
-
   res.on('finish', () => {
-    // Ambil rute spesifik jika ada, kalau tidak gunakan path asli
     const route = req.route ? req.route.path : req.path;
-
-    httpRequestCounter.inc({
-      method: req.method,
-      route: route,
-      status_code: res.statusCode,
-    });
-
-    endTimer({
-      method: req.method,
-      route: route,
-      status_code: res.statusCode,
-    });
+    httpRequestCounter.inc({ method: req.method, route: route, status_code: res.statusCode });
+    endTimer({ method: req.method, route: route, status_code: res.statusCode });
   });
-
   next();
 });
 
@@ -57,6 +60,11 @@ app.get('/', (req, res) => {
 
 app.get('/api/v1/health', (req, res) => {
   res.json({ status: 'ok', service: 'backend', timestamp: new Date().toISOString() });
+});
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
 });
 
 app.use('/api/v1/users', userRoutes);
@@ -69,7 +77,6 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use((err, req, res, _next) => {
   const statusCode = res.statusCode && res.statusCode !== 200 ? res.statusCode : 500;
-
   res.status(statusCode).json({
     message: err.message || 'Internal Server Error',
   });
